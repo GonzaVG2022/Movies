0b50a5e53fbf2919a0a80d848d706692c48bc011
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-03-03 19:25:11 -0300

    inter

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const main = async() => {
     try{
 await sequelize.authenticate();
 console.log("DB connected!");
-sequelize.sync();//{alter:true}
+sequelize.sync();//,{force:true}{alter:true}
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
     } catch (error) {
